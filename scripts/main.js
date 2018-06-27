7ce4518a8f9829c99dd932796038f01bd91c5a18
erickson-devin <devinjohn15@gmail.com>
2018-06-27 16:56:56 -0400

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,13 @@
+
+
+document.addEventListener('DOMContentLoaded', function() {
+
+
+  const HEADER  = document.getElementsByTagName("header")[0];
+  HEADER.addEventListener("click", function(){
+ window.open("index.html","_self");
+})
+
+
+
+}, false);
